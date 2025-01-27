8edc06ff3a0dea9e0aa71b283d705680e4b0883d
Huseyin OZCAKIR <ozcakirh@hotmail.com>
2025-01-27 23:24:41 +0300

    Parametre Giriş Ekran tasarımı öncesi

--- lib/main.dart
+++ lib/main.dart
@@ -46,48 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         drawer: Sizer(
           builder: (context, orientation, deviceType) {
-            return Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  SizedBox(
-                    width: 10.w,
-                    height: 16.h,
-                    child: DrawerHeader(
-                      //             EdgeInsetsGeometry? margin = const EdgeInsets.only(bottom: 8.0),
-                      // EdgeInsetsGeometry padding = const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-                      // Duration duration = const Duration(milliseconds: 250),
-                      margin: const EdgeInsets.only(bottom: 8.0),
-                      //padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-                      //padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
-                      duration: const Duration(milliseconds: 250),
-                      decoration: const BoxDecoration(
-                        color: Colors.deepPurple, // Colors.blue,
-                        shape: BoxShape.rectangle,
-                      ),
-                      child: Text(
-                        'İşlemler',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30.sp, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: const Text('Komisyon Giriş'),
-                    onTap: () {
-                      // Update the state of the app.
-                      // ...
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('Programdan Çıkış'),
-                    onTap: () {
-                      exit(0);
-                    },
-                  ),
-                ],
-              ),
-            );
+            return const AnaMenuCanli();
           },
         ), // Menü
         extendBody: true,
@@ -109,6 +68,139 @@ class _MyHomePageState extends State<MyHomePage> {
         //   child: const Icon(Icons.add),
         // ),
         // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class AnaMenu extends StatelessWidget {
+  const AnaMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return const Placeholder();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          SizedBox(
+            width: 10.w,
+            height: 16.h,
+            child: DrawerHeader(
+              //             EdgeInsetsGeometry? margin = const EdgeInsets.only(bottom: 8.0),
+              // EdgeInsetsGeometry padding = const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              // Duration duration = const Duration(milliseconds: 250),
+              margin: const EdgeInsets.only(bottom: 8.0),
+              //padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              //padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
+              duration: const Duration(milliseconds: 250),
+              decoration: const BoxDecoration(
+                color: Colors.deepPurple, // Colors.blue,
+                shape: BoxShape.rectangle,
+              ),
+              child: Text(
+                'İşlemler',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30.sp, color: Colors.white),
+              ),
+            ),
+          ),
+          ListTile(
+            hoverColor: Colors.amber,
+            focusColor: Colors.red,
+            textColor: Colors.blue,
+            selectedColor: Colors.red,
+            mouseCursor: MouseCursor.defer,
+            autofocus: true,
+            title: const Text(
+              'Komisyon Giriş',
+              style: TextStyle(fontSize: 20),
+            ),
+            //subtitle: const Text('sub Komisyon Giriş'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text(
+              'Programdan Çıkış',
+              style: TextStyle(fontSize: 25),
+            ),
+            onTap: () {
+              exit(0);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnaMenuCanli extends StatefulWidget {
+  const AnaMenuCanli({super.key});
+
+  @override
+  State<AnaMenuCanli> createState() => _AnaMenuCanliState();
+}
+
+class _AnaMenuCanliState extends State<AnaMenuCanli> {
+  @override
+  Widget build(BuildContext context) {
+    //return const Placeholder();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          SizedBox(
+            width: 10.w,
+            height: 16.h,
+            child: DrawerHeader(
+              //             EdgeInsetsGeometry? margin = const EdgeInsets.only(bottom: 8.0),
+              // EdgeInsetsGeometry padding = const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              // Duration duration = const Duration(milliseconds: 250),
+              margin: const EdgeInsets.only(bottom: 8.0),
+              //padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              //padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
+              duration: const Duration(milliseconds: 250),
+              decoration: const BoxDecoration(
+                color: Colors.deepPurple, // Colors.blue,
+                shape: BoxShape.rectangle,
+              ),
+              child: Text(
+                'İşlemler',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30.sp, color: Colors.white),
+              ),
+            ),
+          ),
+          ListTile(
+            hoverColor: Colors.amber,
+            focusColor: Colors.red,
+            textColor: Colors.blue,
+            selectedColor: Colors.red,
+            mouseCursor: MouseCursor.defer,
+            autofocus: true,
+            title: const Text(
+              'Komisyon Giriş',
+              style: TextStyle(fontSize: 20),
+            ),
+            //subtitle: const Text('sub Komisyon Giriş'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text(
+              'Programdan Çıkış',
+              style: TextStyle(fontSize: 25),
+            ),
+            onTap: () {
+              exit(0);
+            },
+          ),
+        ],
       ),
     );
   }
